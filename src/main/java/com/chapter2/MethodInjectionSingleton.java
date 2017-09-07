0b819447fa45bc5986d08df015be32867704eda7
Rekha Rokkam <rekha.rokkam@yahoo.com>
2017-09-07 16:23:38 -0700

    changed some notes

--- src/main/java/com/chapter2/MethodInjectionSingleton.java
+++ src/main/java/com/chapter2/MethodInjectionSingleton.java
@@ -2,6 +2,8 @@ package com.chapter2;
 
 /*
  * Spring will provide a proxy class that implements the abstract method required for method injection. Call to that method happens on the proxy class
+ * 
+ * Method Injection should be used is when a Prototype bean needs to used within a Singleton
  */
 public abstract class MethodInjectionSingleton {
 	
